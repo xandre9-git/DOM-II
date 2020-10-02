4fbfdcecc7e39d53ff3717f6ccf271ad86d69ee5
xandre9-git <jmjames673@gmail.com>
2020-10-01 22:04:59 -0400

    added a mouseover event for first h2 element and set body background color to change when hitting the up arrow or down arrow keys

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+const h2check = document.querySelectorAll('h2')
+console.log(h2check[0])
+
+h2check[0].addEventListener("mouseenter", () => {
+    h2check[0].style.color = 'dodgerblue'
+});
+
+const keyDownBgColorToggle = document.querySelector('body')
+console.log(keyDownBgColorToggle)
+keyDownBgColorToggle.addEventListener("keydown", (e) => {
+    if (e.key === 'ArrowDown') {
+        keyDownBgColorToggle.style.backgroundColor = "yellow"
+    } if (e.key === 'ArrowUp') {
+        keyDownBgColorToggle.style.backgroundColor = "orange"
+    } else {
+        
+    }
+});
